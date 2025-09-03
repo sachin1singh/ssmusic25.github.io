5eee100f43808e0ef34db860adada6438ac45093
Sachin Singh <86557710+sachin1singh@users.noreply.github.com>
2025-09-03 14:30:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-// Example JS - you can expand later (animations, effects, etc.)
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("SS Music Records site loaded successfully!");
-});
+
